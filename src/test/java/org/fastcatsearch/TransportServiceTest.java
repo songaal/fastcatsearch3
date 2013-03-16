dac236d5dd955931eacaf08507c85f37500883c3
songaal <swsong_dev@websqrd.com>
2013-03-16 22:22:16 +0900

    test

---
+++ src/test/java/org/fastcatsearch/TransportServiceTest.java
@@ -0,0 +1,14 @@
+package org.fastcatsearch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransportServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
