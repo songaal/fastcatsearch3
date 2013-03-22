38c302b1badcc67289a15fb6541063ed465cd4bd
lupfeliz <lupfeliz@gmail.com>
2013-03-22 10:42:41 +0000

    add command exception class

---
+++ src/main/java/org/fastcatsearch/cli/CommandException.java
@@ -0,0 +1,12 @@
+package org.fastcatsearch.cli;
+
+public class CommandException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4168010294735694200L;
+	
+	public static final int SYNTAX_ERROR = 1;
+
+}
