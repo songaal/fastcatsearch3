b5ab23ce9d5a70ab3a4b138a18c0256f65e0b2e2
lupfeliz <lupfeliz@gmail.com>
2013-04-17 14:41:08 +0900

    1. add search action command (draft)

--- src/main/java/org/fastcatsearch/cli/command/SearchActionCommand.java
+++ src/main/java/org/fastcatsearch/cli/command/SearchActionCommand.java
@@ -1,13 +1,24 @@
 package org.fastcatsearch.cli.command;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.fastcatsearch.cli.Command;
 import org.fastcatsearch.cli.CommandException;
 import org.fastcatsearch.cli.CommandResult;
 import org.fastcatsearch.cli.ConsoleSessionContext;
+import org.fastcatsearch.control.JobController;
+import org.fastcatsearch.control.JobResult;
+import org.fastcatsearch.ir.query.Result;
+import org.fastcatsearch.ir.query.Row;
+import org.fastcatsearch.job.SearchJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SearchActionCommand extends Command {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SearchActionCommand.class);
 
 	@Override
 	public boolean isCommand(String[] cmd) {
@@ -17,8 +28,36 @@ public class SearchActionCommand extends Command {
 	@Override
 	public CommandResult doCommand(String[] cmd, ConsoleSessionContext context)
 			throws IOException, CommandException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String msg = "";
+		StringBuilder queryBuffer = new StringBuilder();
+		
+		for(int inx=1;inx<cmd.length;inx++) {
+			queryBuffer.append(cmd[inx]);
+		}
+		
+		SearchJob job = new SearchJob();
+		job.setArgs(new String[] { queryBuffer.toString() });
+		Result result = null;
+		JobResult jobResult = JobController.getInstance().offer(job);
+		Object obj = jobResult.poll(100);
+		
+		if(jobResult.isSuccess()) {
+			result = (Result)obj;
+			int columns = result.getFieldCount();
+			String[] header = result.getFieldNameList();
+			List<Object[]> data = new ArrayList<Object[]>();
+			for(Row row : result.getData()) {
+				Object[] record = new Object[columns];
+				for(int colInx=0;colInx < columns; colInx++) {
+					record[colInx] = new String(row.get(colInx));
+				}
+				data.add(record);
+			}
+			msg = super.printData(data, header);
+			return new CommandResult(msg, CommandResult.Status.SUCCESS);
+		}
+		return new CommandResult(msg, CommandResult.Status.FAIL);
 	}
 
 }
