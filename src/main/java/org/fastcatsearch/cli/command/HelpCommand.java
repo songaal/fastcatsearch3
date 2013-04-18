fae339499e2bad325901f250e817215ad14f3da4
jaeho-kang <greennuri@gmail.com>
2013-04-18 18:28:05 +0900

    help

---
+++ src/main/java/org/fastcatsearch/cli/command/HelpCommand.java
@@ -0,0 +1,36 @@
+package org.fastcatsearch.cli.command;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.fastcatsearch.cli.Command;
+import org.fastcatsearch.cli.CommandException;
+import org.fastcatsearch.cli.CommandResult;
+import org.fastcatsearch.cli.ConsoleSessionContext;
+import org.fastcatsearch.db.object.IndexingResult;
+import org.fastcatsearch.ir.config.IRConfig;
+import org.fastcatsearch.ir.config.IRSettings;
+import org.fastcatsearch.ir.util.Formatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelpCommand extends Command {
+	
+	private static Logger logger = LoggerFactory.getLogger(HelpCommand.class);
+	
+	@Override
+	public boolean isCommand(String[] cmd) {
+		return isCommand(CMD_HELP, cmd);
+	}
+
+	@Override
+	public CommandResult doCommand(String[] cmd, ConsoleSessionContext context) throws IOException, CommandException {
+		return new CommandResult("now contructing ^^ may be tomorrow~ ", CommandResult.Status.SUCCESS);
+	}
+
+	private void addRecord(List<Object[]> data, String key, String value) {
+		data.add(new Object[] { key, value });
+	}
+
+}
