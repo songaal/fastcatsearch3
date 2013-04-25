82f429a03cfe5ad43e5a6d3df1ad74df76da4b75
lupfeliz <lupfeliz@gmail.com>
2013-04-25 20:38:30 +0900

    1. streamable document list

--- src/main/java/org/fastcatsearch/transport/vo/StreamableDocumentList.java
+++ src/main/java/org/fastcatsearch/transport/vo/StreamableDocumentList.java
@@ -6,12 +6,15 @@ import java.util.List;
 import org.fastcatsearch.common.io.StreamInput;
 import org.fastcatsearch.common.io.StreamOutput;
 import org.fastcatsearch.common.io.Streamable;
+import org.fastcatsearch.ir.config.Field;
 import org.fastcatsearch.ir.document.Document;
 
 public class StreamableDocumentList implements Streamable {
+	
+	private List<Document> documentList;
 
 	public StreamableDocumentList(List<Document> documentList){
-		
+		this.documentList = documentList;
 	}
 	
 	@Override
@@ -22,8 +25,23 @@ public class StreamableDocumentList implements Streamable {
 
 	@Override
 	public void writeTo(StreamOutput output) throws IOException {
+		int length = documentList.size();
+		output.writeInt(length);
+		for(int documentInx=0;documentInx<length;documentInx++) {
+			Document document = documentList.get(documentInx);
+			int fieldSize = document.size();
+			int score = document.getScore();
+			output.writeInt(fieldSize);
+			output.writeInt(score);
+			for(int fieldInx=0;fieldInx<fieldSize;fieldInx++) {
+				Field field = document.get(fieldInx);
+			}
+		}
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	public List<Document> getDocumentList() {
+		return documentList;
+	}
 }
