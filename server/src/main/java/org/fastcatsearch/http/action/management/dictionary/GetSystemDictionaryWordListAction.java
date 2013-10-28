0eb836c9d08f1a6b2cdd71fa96a25b183b487c37
swsong-dev@websqrd.com <swsong-dev@websqrd.com>
2013-10-28 17:24:27 +0900

    시스템 사전 조회기능.

---
+++ server/src/main/java/org/fastcatsearch/http/action/management/dictionary/GetSystemDictionaryWordListAction.java
@@ -0,0 +1,56 @@
+package org.fastcatsearch.http.action.management.dictionary;
+
+import java.io.Writer;
+import java.util.List;
+
+import org.fastcatsearch.http.ActionMapping;
+import org.fastcatsearch.http.action.ActionRequest;
+import org.fastcatsearch.http.action.ActionResponse;
+import org.fastcatsearch.http.action.AuthAction;
+import org.fastcatsearch.ir.io.CharVector;
+import org.fastcatsearch.plugin.Plugin;
+import org.fastcatsearch.plugin.PluginService;
+import org.fastcatsearch.plugin.analysis.AnalysisPlugin;
+import org.fastcatsearch.plugin.analysis.AnalysisPluginSetting;
+import org.fastcatsearch.plugin.analysis.AnalysisPluginSetting.DictionarySetting;
+import org.fastcatsearch.service.ServiceManager;
+import org.fastcatsearch.util.ResponseWriter;
+
+@ActionMapping("/management/dictionary/system")
+public class GetSystemDictionaryWordListAction extends AuthAction {
+
+	@Override
+	public void doAuthAction(ActionRequest request, ActionResponse response) throws Exception {
+		
+		String pluginId = request.getParameter("pluginId");
+		String search = request.getParameter("search");
+		
+		PluginService pluginService = ServiceManager.getInstance().getService(PluginService.class);
+		Plugin plugin = pluginService.getPlugin(pluginId);
+		AnalysisPlugin analysisPlugin = (AnalysisPlugin) plugin;
+		
+		AnalysisPluginSetting analysisPluginSetting = (AnalysisPluginSetting) plugin.getPluginSetting();
+		List<DictionarySetting> dictionaryList = analysisPluginSetting.getDictionarySettingList();
+		
+		CharVector token = new CharVector(search);
+		List<?> list = analysisPlugin.getDictionary().find(token);
+		
+		Writer writer = response.getWriter();
+		ResponseWriter resultWriter = getDefaultResponseWriter(writer);
+		
+		resultWriter.object().key("list").array();
+		
+		if(list != null){
+			for(Object obj : list){
+				resultWriter.value(obj.toString());
+			}
+		}
+		
+		resultWriter.endArray();
+		
+		resultWriter.endObject();
+		resultWriter.done();
+			
+	}
+
+}
