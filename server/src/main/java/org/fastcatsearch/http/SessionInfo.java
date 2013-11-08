aa3e665cb29a501d02d4b547ed477e9832c5d701
swsong-dev@websqrd.com <swsong-dev@websqrd.com>
2013-11-08 18:22:45 +0900

    세션인포.

---
+++ server/src/main/java/org/fastcatsearch/http/SessionInfo.java
@@ -0,0 +1,45 @@
+package org.fastcatsearch.http;
+
+import java.util.Map;
+
+public class SessionInfo {
+	
+	private String userId;
+	private Map<ActionAuthority, ActionAuthorityLevel> authorityMap;
+	
+	public SessionInfo(String userId, Map<ActionAuthority, ActionAuthorityLevel> authorityMap){
+		this.userId = userId;
+		this.authorityMap = authorityMap;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Map<ActionAuthority, ActionAuthorityLevel> getAuthorityMap() {
+		return authorityMap;
+	}
+
+	public void setAuthorityMap(Map<ActionAuthority, ActionAuthorityLevel> authorityMap) {
+		this.authorityMap = authorityMap;
+	}
+	
+	public boolean hasAuthority(ActionAuthority actionAuthority, ActionAuthorityLevel actionAuthorityLevel){
+		if(actionAuthority == null || actionAuthorityLevel == null){
+			return false;
+		}
+		
+		ActionAuthorityLevel groupAuthoritylevel = authorityMap.get(actionAuthority);
+		if(groupAuthoritylevel != null){
+			return groupAuthoritylevel.isLargerThan(actionAuthorityLevel);
+		}
+		
+		//if authority is not defined, it has no authority.
+		return false;
+	}
+	
+}
