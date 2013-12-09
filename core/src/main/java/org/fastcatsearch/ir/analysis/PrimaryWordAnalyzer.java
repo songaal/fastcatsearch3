43711e36ac778ccd9765d04195bf4a74d7eb36c6
swsong-dev@websqrd.com <swsong-dev@websqrd.com>
2013-12-09 14:48:35 +0900

    PrimaryWorkAnalyzer로 이름변경. Test Case 작성.

--- core/src/main/java/org/fastcatsearch/ir/analysis/PrimaryWordAnalyzer.java
+++ core/src/main/java/org/fastcatsearch/ir/analysis/PrimaryWordAnalyzer.java
@@ -1,6 +1,7 @@
 package org.fastcatsearch.ir.analysis;
 
 import java.io.IOException;
+
 import java.io.Reader;
 
 import org.apache.lucene.analysis.Analyzer;
@@ -11,42 +12,44 @@ import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NormalizedKeywordAnalyzer extends Analyzer {
-	
-	private static final Logger logger = LoggerFactory.getLogger(NormalizedKeywordAnalyzer.class);
-	
-	public NormalizedKeywordAnalyzer() {
+/**
+ * 공백과 특수문자를 제외한 주요 단어만 뽑아낸다. 사전과 분석로직은 사용하지 않는다.
+ * */
+public class PrimaryWordAnalyzer extends Analyzer {
+
+	private static final Logger logger = LoggerFactory.getLogger(PrimaryWordAnalyzerTest.class);
+
+	public PrimaryWordAnalyzer() {
 	}
 
 	@Override
-	protected TokenStreamComponents createComponents(String fieldName,
-			Reader reader) {
-		
+	protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
+
 		final TypeTokenizer tokenizer = new TypeTokenizer(reader);
 		try {
 			tokenizer.reset();
 		} catch (IOException e) {
-			logger.error("tokenizer reset error",e );
+			logger.error("tokenizer reset error", e);
 		}
-		//분리된 어절을 하나씩 처리한다.
-		
+		// 분리된 어절을 하나씩 처리한다.
+
 		final CharTermAttribute charTermAttribute = tokenizer.getAttribute(CharTermAttribute.class);
 		final TypeAttribute typeAttribute = tokenizer.getAttribute(TypeAttribute.class);
 
-		TokenFilter filter = new TokenFilter(tokenizer)  {
+		TokenFilter filter = new TokenFilter(tokenizer) {
 
 			@Override
 			public boolean incrementToken() throws IOException {
-				while(input.incrementToken()) {
-					if(typeAttribute.type()!=TypeTokenizer.SYMBOL) {
-						logger.debug("term : {}", charTermAttribute.toString());
+				while (input.incrementToken()) {
+					if (typeAttribute.type() != TypeTokenizer.SYMBOL) {
+						// logger.debug("term : {}", charTermAttribute.toString());
 						return true;
 					}
 				}
 				return false;
 			}
 		};
-		
-		return new TokenStreamComponents(tokenizer,filter);
+
+		return new TokenStreamComponents(tokenizer, filter);
 	}
 }
