50ca02553cdfec4476c84920a5e34fa2d8eac023
lupfeliz <lupfeliz@gmail.com>
2013-12-12 09:50:40 +0900

    1. 필터테스트 정비

--- core/src/test/java/org/fastcatsearch/ir/filter/FilterTest.java
+++ core/src/test/java/org/fastcatsearch/ir/filter/FilterTest.java
@@ -3,34 +3,26 @@ package org.fastcatsearch.ir.filter;
 import static org.junit.Assert.*;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-
-import org.apache.lucene.store.RAMFile;
-import org.apache.lucene.store.RAMInputStream;
-import org.apache.lucene.store.RAMOutputStream;
+import org.apache.commons.io.FileUtils;
 import org.fastcatsearch.ir.common.IRException;
-import org.fastcatsearch.ir.io.BufferedFileInput;
+import org.fastcatsearch.ir.document.Document;
+import org.fastcatsearch.ir.field.Field;
+import org.fastcatsearch.ir.index.FieldIndexWriter;
+import org.fastcatsearch.ir.io.CharVector;
 import org.fastcatsearch.ir.io.DataRef;
-import org.fastcatsearch.ir.io.IndexInput;
-import org.fastcatsearch.ir.io.IndexOutput;
-import org.fastcatsearch.ir.io.StreamInputRef;
 import org.fastcatsearch.ir.query.Filter;
 import org.fastcatsearch.ir.query.RankInfo;
 import org.fastcatsearch.ir.search.FieldIndexReader;
-import org.fastcatsearch.ir.search.FieldIndexesReader;
-import org.fastcatsearch.ir.search.IndexRef;
 import org.fastcatsearch.ir.settings.FieldIndexSetting;
 import org.fastcatsearch.ir.settings.FieldSetting;
 import org.fastcatsearch.ir.settings.FieldSetting.Type;
-import org.fastcatsearch.ir.settings.Schema;
-import org.fastcatsearch.ir.settings.SchemaSetting;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,91 +31,124 @@ public class FilterTest {
 	
 	private static final Logger logger = LoggerFactory.getLogger(FilterTest.class);
 	
-	public void testReader() throws IOException {
+	public void testSingleReadWriteAndFilterOnTheFly(String fieldId, 
+			int filterType, Type fieldType, int fieldSize,
+			List<Object> fieldData, String pattern, String endPattern, 
+			int boostScore, List<Boolean> filterResult, List<Integer> boostResult) 
+					throws IOException, IRException {
 		
-		File file = new File("/home/lupfeliz/Documents/joara/collections/book/data/index0/0");
-		IndexInput dataInput = new BufferedFileInput(new File(file,"field.redate.index"));
+		RankInfo rankInfo = new RankInfo();
 		
+		File dir = File.createTempFile("dir", ".tmp");
+		dir.delete();
+		dir.mkdir();
+		FieldSetting fieldSetting = new FieldSetting(fieldId,"",fieldType);
 		
+		if(fieldSize>0) {
+			fieldSetting.setSize(fieldSize);
+		}
+		
+		FieldIndexSetting fieldIndexSetting = new FieldIndexSetting(fieldId, 
+				fieldId, fieldId, fieldSetting.getSize(), false);
+		Map<String, FieldSetting> fieldSettingMap = 
+				new HashMap<String, FieldSetting>();
+		Map<String, Integer> fieldSequenceMap = 
+				new HashMap<String, Integer>();
+		fieldSettingMap.put(fieldId.toUpperCase(), fieldSetting);
+		fieldSequenceMap.put(fieldId.toUpperCase(), 0);
+		
+		FieldIndexWriter writer = new FieldIndexWriter(fieldIndexSetting,
+				fieldSettingMap,fieldSequenceMap, dir);
+		
+		for(int docNo=0; docNo<fieldData.size(); docNo++) {
+			Field field = fieldSetting.createEmptyField();
+			field.setFieldsData(fieldData.get(docNo));
+			Document document = new Document(docNo);
+			document.add(field);
+			writer.write(document);
+		}
+		writer.close();
+		
+		Filter filter = new Filter(fieldId, Filter.MATCH, pattern,endPattern,boostScore);
+		FilterFunction func = filter.createFilterFunction(fieldIndexSetting, fieldSetting);
+		
+		FieldIndexReader reader = new FieldIndexReader(fieldIndexSetting,
+				fieldSettingMap, dir );
+		DataRef dataRef = reader.getRef();
+		
+		for(int docNo=0;docNo<fieldData.size();docNo++) {
+			reader.read(docNo);
+			filterResult.add(func.filtering(rankInfo, dataRef));
+			boostResult.add(rankInfo.hit());
+		}
+		reader.close();
+		FileUtils.forceDelete(dir);
+	}
+	
+	@Test
+	public void testMatchFilterOnTheFly() throws IOException, IRException {
+		
+		String fieldId = "CATEGORY";
+		int filterType = Filter.MATCH;
+		Type fieldType = Type.ASTRING;
+		List<Object> fieldData = null;
+		String pattern = "01001";
+		String endPattern = "";
+		int boostScore = 0;
+		List<Boolean> filterResult = new ArrayList<Boolean>();
+		List<Integer> boostResult = new ArrayList<Integer>();
+		
+		fieldData = Arrays.asList(new Object[] {
+			"00000", "00001", "01001", "00004"
+		});
+		
+		for(int inx=0;inx<fieldData.size();inx++) {
+			fieldData.set(inx, 
+					new CharVector((String)fieldData.get(inx)));
+		}
+		
+		testSingleReadWriteAndFilterOnTheFly(fieldId, filterType, 
+			fieldType, 5, fieldData, pattern, 
+			endPattern, boostScore, filterResult,
+			boostResult);
+		
+		logger.debug("filter result : {}", filterResult);
+		
+		List<Boolean> actual = Arrays.asList(new Boolean[] {
+				false,false,true,false
+		});
+		
+		org.junit.Assert.assertEquals(filterResult, actual);
 	}
 
 	@Test
-	public void testMatchFilter() throws IOException, IRException, JAXBException {
+	public void testSectionFilterOnTheFly() throws IOException, IRException {
 		
-		String fieldId = "REDATE";
+		String fieldId = "CNT_READ";
+		int filterType = Filter.SECTION;
+		Type fieldType = Type.INT;
+		List<Object> fieldData = null;
+		String pattern = "128";
+		String endPattern = "256";
+		int boostScore = 0;
+		List<Boolean> filterResult = new ArrayList<Boolean>();
+		List<Integer> boostResult = new ArrayList<Integer>();
 		
-		InputStream is = new FileInputStream("/home/lupfeliz/Documents/joara/collections/book/schema.xml");
-		SchemaSetting setting = readConfig(is, SchemaSetting.class);
-		is.close();
+		fieldData = Arrays.asList(new Object[] {
+			1, 999, 130, 1024 
+		});
 		
-		Schema schema = new Schema(setting);
+		testSingleReadWriteAndFilterOnTheFly(fieldId, filterType, 
+			fieldType, 5, fieldData, pattern, 
+			endPattern, boostScore, filterResult,
+			boostResult);
 		
-		File file = new File("/home/lupfeliz/Documents/joara/collections/book/data/index0/0");
+		logger.debug("filter result : {}", filterResult);
 		
-		FieldIndexesReader readers = new FieldIndexesReader(schema, file);
+		List<Boolean> actual = Arrays.asList(new Boolean[] {
+				false,false,true,false
+		});
 		
-		String[] fieldList = new String[]{fieldId};
-		
-		IndexRef<FieldIndexReader> fieldIndexRef= readers.selectIndexRef(fieldList);
-		
-		List<DataRef> dataRefList = fieldIndexRef.getDataRefList();
-		
-		FieldIndexReader reader = fieldIndexRef.getReader(0);
-		
-		Filter filter = new Filter(fieldId, Filter.MATCH, "20010323232736000","",0);
-		
-		List<FieldSetting>fieldSettingList = setting.getFieldSettingList();		
-		FieldSetting fieldSetting = null;
-		for(int inx=0;inx<fieldSettingList.size();inx++) {
-			if(fieldId.equalsIgnoreCase(fieldSettingList.get(inx).getId())) {
-				logger.debug("found fieldSetting..");
-				fieldSetting = fieldSettingList.get(inx);
-			}
-		}
-		
-		List<FieldIndexSetting>fieldIndexSettingList = setting.getFieldIndexSettingList();
-		FieldIndexSetting fieldIndexSetting = null;
-		for(int inx=0;inx<fieldIndexSettingList.size();inx++) {
-			if(fieldId.equalsIgnoreCase(fieldIndexSettingList.get(inx).getId())) {
-				logger.debug("found fieldIndexSetting..");
-				fieldIndexSetting = fieldIndexSettingList.get(inx);
-			}
-		}
-		
-		FilterFunction filterFunction = filter.createFilterFunction(fieldIndexSetting, fieldSetting);
-		
-		RankInfo rankInfo = new RankInfo();
-		DataRef dataRef = dataRefList.get(0);
-		
-		for(int inx=0;inx<10;inx++) {
-		
-			reader.read(inx);
-			boolean bool = filterFunction.filtering(rankInfo, dataRef);
-			logger.debug("result:{}", bool);
-		}
-		
-//		RAMFile f = new RAMFile();
-//		IndexOutput outptut = new RAMOutputStream(f);
-//		IndexInput input = new RAMInputStream("test",f);
-//		DataRef dataRef = new StreamInputRef(input,4);
-//		RankInfo rankInfo = new RankInfo();
-//		FieldIndexSetting fieldIndexSetting = new FieldIndexSetting("test", "test", "test", 4, false);
-//		FieldSetting fieldSetting = new FieldSetting("test", "test", Type.INT);
-//		FilterFunction filterFunction = filter.createFilterFunction(fieldIndexSetting, fieldSetting);
-//		filterFunction.filtering(rankInfo, dataRef);
-		//fail("Not yet implemented");
+		org.junit.Assert.assertEquals(filterResult, actual);
 	}
-	
-	public static <T> T readConfig(InputStream is, Class<T> jaxbConfigClass) throws JAXBException {
-		if(is == null){
-			return null;
-		}
-		JAXBContext context = JAXBContext.newInstance(jaxbConfigClass);
-		
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-		
-		T config = (T) unmarshaller.unmarshal(is);
-		return config;
-	}
-
 }
