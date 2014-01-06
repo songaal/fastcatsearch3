25bd82357fc7ed30b02217accd940a7f2de097e3
Yongmo Jeong <lupfeliz@gmail.com>
2014-01-06 19:55:28 +0900

    1. 초성검색용 메소드 구현

--- core/src/main/java/org/fastcatsearch/util/lang/ko/HangulUtil.java
+++ core/src/main/java/org/fastcatsearch/util/lang/ko/HangulUtil.java
@@ -107,7 +107,7 @@ public class HangulUtil {
 	}
 	
 	// 초성검색
-	public static String makeChosung(String keyword, char delimiter) {
+	public static String makeHangulChosung(String keyword, char delimiter) {
 		StringBuffer candidate = new StringBuffer();
 		StringBuffer prefix = new StringBuffer();
 		for (int i = 0; i < keyword.length(); i++) {
@@ -116,30 +116,14 @@ public class HangulUtil {
 				prefix.append(ch);
 				candidate.append(prefix);
 			} else {
-				// Character is composed of {Chosung+Jungsung} OR
-				// {Chosung+Jungsung+Jongsung}
 				int unicode = ch - unicodeHangulBase;
 				int choSung = unicode / (JUNGSUNG_LIST.length() * JONGSUNG_LIST.length());
-				// 1. add prefix+chosung
 				candidate.append(prefix);
 				candidate.append(CHOSUNG_LIST.charAt(choSung));
 				candidate.append(delimiter);
-				// 2. add prefix+chosung+jungsung
-				unicode = unicode % (JUNGSUNG_LIST.length() * JONGSUNG_LIST.length());
-				int jongSung = unicode % JONGSUNG_LIST.length();
-				char choJung = (char) (ch - jongSung);
-				candidate.append(prefix);
-				candidate.append(choJung);
-				// change prefix
-				prefix.append(ch);
-				if (jongSung > 0) {
-					candidate.append(delimiter);
-					// 3. add whole character
-					candidate.append(prefix);
-				}
+				
+				prefix.append(CHOSUNG_LIST.charAt(choSung));
 			}
-			if (i < keyword.length() - 1)
-				candidate.append(delimiter);
 		}
 		return candidate.toString();
 	}
