c7b529f90336b9af47a5e242894ff29aae9bdadd
swsong-dev@websqrd.com <swsong-dev@websqrd.com>
2014-01-13 21:20:00 +0900

    사전 데이터 reset 업데이트 URI변경. /management/dictionary/reset-data로..

--- server/src/main/java/org/fastcatsearch/http/action/management/dictionary/ResetDictionaryDataAction.java
+++ server/src/main/java/org/fastcatsearch/http/action/management/dictionary/ResetDictionaryDataAction.java
@@ -20,17 +20,15 @@ import org.fastcatsearch.plugin.analysis.AnalysisPluginSetting.ColumnSetting;
 import org.fastcatsearch.service.ServiceManager;
 import org.fastcatsearch.util.ResponseWriter;
 
-@ActionMapping(value="/management/dictionary/sync-map-dictionary-key", authority=ActionAuthority.Dictionary, authorityLevel=ActionAuthorityLevel.WRITABLE)
-public class SynchronizeMapDictionaryKeyAction extends AuthAction {
+@ActionMapping(value = "/management/dictionary/reset-data", authority = ActionAuthority.Dictionary, authorityLevel = ActionAuthorityLevel.WRITABLE)
+public class ResetDictionaryDataAction extends AuthAction {
 
 	@Override
 	public void doAuthAction(ActionRequest request, ActionResponse response) throws Exception {
 		/**
-		 * 맵사전 재구축, http post 로 받아오는 데이터를 기반으로 사전 재구축
-		 * 기존데이터는 삭제하고 입력.
+		 * 맵사전 재구축, http post 로 받아오는 데이터를 기반으로 사전 재구축 기존데이터는 삭제하고 입력.
 		 * 
-		 * TODO:만약 사전데이터가 아주 크다면 Stream 형식을 고려 하도록 한다. FIXME:맵사전에 한해 스키마가 바뀌지
-		 * 않는다고 가정, 하드코딩 함.
+		 * TODO:만약 사전데이터가 아주 크다면 Stream 형식을 고려 하도록 한다. FIXME:맵사전에 한해 스키마가 바뀌지 않는다고 가정, 하드코딩 함.
 		 */
 
 		int status = 1;
@@ -48,37 +46,37 @@ public class SynchronizeMapDictionaryKeyAction extends AuthAction {
 
 		@SuppressWarnings("rawtypes")
 		AnalysisPlugin analysisPlugin = (AnalysisPlugin) plugin;
-		
+
 		MapperSession<DictionaryMapper> mapperSession = null;
-		
+
 		try {
 
 			DictionaryDAO dictionaryDAO = analysisPlugin.getDictionaryDAO(dictionaryId);
 			List<ColumnSetting> columnSettingList = dictionaryDAO.columnSettingList();
-			
+
 			String[] columns = new String[columnSettingList.size()];
-			
-			for(int colInx=0;colInx<columnSettingList.size();colInx++) {
+
+			for (int colInx = 0; colInx < columnSettingList.size(); colInx++) {
 				ColumnSetting columnSetting = columnSettingList.get(colInx);
 				columns[colInx] = columnSetting.getName().toUpperCase();
 			}
-			
+
 			String tableName = dictionaryDAO.getTableName();
-			
+
 			mapperSession = dictionaryDAO.openMapperSession();
-			
+
 			mapperSession.getMapper().truncate(tableName);
-			
-			for(int recordInx=0;recordInx<dataArray.length;recordInx++) {
+
+			for (int recordInx = 0; recordInx < dataArray.length; recordInx++) {
 				String recordStr = dataArray[recordInx];
-				
+
 				String[] record = recordStr.split("\t");
 				String[] values = new String[columns.length];
 				System.arraycopy(record, 0, values, 0, record.length);
-				
+
 				mapperSession.getMapper().putEntry(tableName, columns, values);
 			}
-			
+
 			mapperSession.commit();
 
 			analysisPlugin.dictionaryStatusDAO().updateUpdateTime(dictionaryId);
@@ -90,9 +88,9 @@ public class SynchronizeMapDictionaryKeyAction extends AuthAction {
 			logger.error("", e);
 		} finally {
 			if (mapperSession != null) {
-				if(status == 0){
+				if (status == 0) {
 					mapperSession.commit();
-				}else{
+				} else {
 					mapperSession.rollback();
 				}
 				mapperSession.closeSession();
