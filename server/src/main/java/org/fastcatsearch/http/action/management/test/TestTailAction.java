53728e10d3befb3373db4617332525dff26a0620
swsong-dev@websqrd.com <swsong-dev@websqrd.com>
2014-01-18 18:24:11 +0900

    tail action 수정.

--- server/src/main/java/org/fastcatsearch/http/action/management/test/TestTailAction.java
+++ server/src/main/java/org/fastcatsearch/http/action/management/test/TestTailAction.java
@@ -5,46 +5,61 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Date;
 
 import org.fastcatsearch.http.ActionMapping;
 import org.fastcatsearch.http.action.ActionRequest;
 import org.fastcatsearch.http.action.ActionResponse;
 import org.fastcatsearch.http.action.AuthAction;
 import org.fastcatsearch.http.action.StreamWriter;
+import org.fastcatsearch.ir.util.Formatter;
 
 /*
  * */
 @ActionMapping("/management/test/tail")
 public class TestTailAction extends AuthAction {
 
+	static String encoding = "utf-8";
+
 	@Override
 	public void doAuthAction(ActionRequest request, ActionResponse response) throws Exception {
 		String filepath = request.getParameter("filepath");
+		int defaultSize = request.getIntParameter("defaultSize", 1024);
+		String type = request.getParameter("type", "text");
+		String linefeed = null;
+		if (type.equalsIgnoreCase("text")) {
+			linefeed = "\n";
+		} else if (type.equalsIgnoreCase("html")) {
+			linefeed = "<br>";
+
+		}
 		File f = new File(filepath);
 		logger.debug("Tail file = {}", f.getAbsolutePath());
-		
+
 		if (!f.exists()) {
 			return;
 		}
-		long fileSize = f.length();
+		long skipSize = f.length();
 		Reader fileReader = new FileReader(f);
-		fileReader.skip(fileSize);
-		
+		if(skipSize > defaultSize){
+			skipSize -= defaultSize;
+		}
+		fileReader.skip(skipSize);
+
 		writeHeader(response);
 		StreamWriter streamWriter = response.getStreamWriter();
-		BufferedReader input = new BufferedReader(fileReader);
+		BufferedReader input = new BufferedReader(fileReader, 1024);
 		try {
 			streamWriter.writeHeader(response);
 
+			printTailInfoHeader(streamWriter, f);
 			String line = null;
 			while (true) {
 				if ((line = input.readLine()) != null) {
-					byte[] data = (line + "\n").getBytes("utf-8");
+					byte[] data = (line + linefeed).getBytes(encoding);
 					streamWriter.write(data, 0, data.length);
-				}else{
+				} else {
 					try {
-						Thread.sleep(1000L);
+						Thread.sleep(500L);
 					} catch (InterruptedException x) {
 						Thread.currentThread().interrupt();
 						break;
@@ -55,7 +70,7 @@ public class TestTailAction extends AuthAction {
 			logger.error("TestTailAction 에러발생. ", e.getMessage());
 			return;
 		} finally {
-			if(input != null){
+			if (input != null) {
 				input.close();
 			}
 			if (streamWriter != null) {
@@ -64,6 +79,14 @@ public class TestTailAction extends AuthAction {
 		}
 
 		logger.debug("TestTailAction Finished!");
+	}
+
+	private void printTailInfoHeader(StreamWriter streamWriter, File f) throws IOException {
+		String header = "####################################################\n" 
+					+ "# Tail file = " + f.getName() + " (" + Formatter.getFormatSize(f.length()) + ")\n"
+				+ "####################################################\n";
+		byte[] str = header.getBytes(encoding);
+		streamWriter.write(str, 0, str.length);
 	}
 
 }
