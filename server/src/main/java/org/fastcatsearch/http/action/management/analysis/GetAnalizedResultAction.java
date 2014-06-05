1feb15bfa29d7ee3acd1b0487809961a6c190c9f
Yongmo Jeong <lupfeliz@gmail.com>
2014-06-05 17:43:14 +0900

    add necessary attributes for GetProductAnalizedResultAction

--- server/src/main/java/org/fastcatsearch/http/action/management/analysis/GetAnalizedResultAction.java
+++ server/src/main/java/org/fastcatsearch/http/action/management/analysis/GetAnalizedResultAction.java
@@ -2,13 +2,18 @@ package org.fastcatsearch.http.action.management.analysis;
 
 import java.io.CharArrayReader;
 import java.io.Writer;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.AdditionalTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.CharsRefTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.StopwordAttribute;
+import org.apache.lucene.analysis.tokenattributes.SynonymAttribute;
 import org.fastcatsearch.http.ActionAuthority;
 import org.fastcatsearch.http.ActionAuthorityLevel;
 import org.fastcatsearch.http.ActionMapping;
@@ -16,11 +21,13 @@ import org.fastcatsearch.http.action.ActionRequest;
 import org.fastcatsearch.http.action.ActionResponse;
 import org.fastcatsearch.http.action.AuthAction;
 import org.fastcatsearch.ir.analysis.AnalyzerPool;
+import org.fastcatsearch.ir.io.CharVector;
 import org.fastcatsearch.plugin.Plugin;
 import org.fastcatsearch.plugin.PluginService;
 import org.fastcatsearch.plugin.analysis.AnalysisPlugin;
 import org.fastcatsearch.service.ServiceManager;
 import org.fastcatsearch.util.ResponseWriter;
+import org.fastcatsearch.util.ResultWriterException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,6 +56,7 @@ public class GetAnalizedResultAction extends AuthAction {
 			if (plugin == null) {
 				throw new Exception("Cannot find plugin >> " + (pluginId + "." + analyzerId));
 			}
+			@SuppressWarnings("rawtypes")
 			AnalysisPlugin analysisPlugin = (AnalysisPlugin) plugin;
 			AnalyzerPool analyzerPool = analysisPlugin.getAnalyzerPool(analyzerId);
 			
@@ -76,6 +84,20 @@ public class GetAnalizedResultAction extends AuthAction {
 					if (tokenStream.hasAttribute(CharsRefTermAttribute.class)) {
 						termAttribute = tokenStream.getAttribute(CharsRefTermAttribute.class);
 					}
+					SynonymAttribute synonymAttribute = null;
+					if(tokenStream.hasAttribute(SynonymAttribute.class)) {
+						synonymAttribute = tokenStream.getAttribute(SynonymAttribute.class);
+					}
+					AdditionalTermAttribute additionalTermAttribute = null;
+					if(tokenStream.hasAttribute(AdditionalTermAttribute.class)) {
+						additionalTermAttribute = tokenStream.getAttribute(AdditionalTermAttribute.class);
+					}
+					
+					StopwordAttribute stopwordAttribute = null;
+					if(tokenStream.hasAttribute(StopwordAttribute.class)) {
+						stopwordAttribute = tokenStream.getAttribute(StopwordAttribute.class);
+					}
+					
 					CharTermAttribute charTermAttribute = tokenStream.getAttribute(CharTermAttribute.class);
 
 					while (tokenStream.incrementToken()) {
@@ -85,7 +107,23 @@ public class GetAnalizedResultAction extends AuthAction {
 						} else {
 							word = charTermAttribute.toString();
 						}
+						
+						//remove stopword
+						if(stopwordAttribute.isStopword()) {
+							continue;
+						}
 						responseWriter.value(word);
+						appendSynonyms(responseWriter, synonymAttribute);
+						
+						//if found additiona term. print it.
+						if(additionalTermAttribute !=null && additionalTermAttribute.size() > 0) {
+							Iterator<String> termIter = additionalTermAttribute.iterateAdditionalTerms();
+							while(termIter.hasNext()) {
+								String token = termIter.next();
+								responseWriter.value(token);
+								appendSynonyms(responseWriter, synonymAttribute);
+							}
+						}
 					}
 					responseWriter.endArray();
 				}
@@ -112,4 +150,25 @@ public class GetAnalizedResultAction extends AuthAction {
 		}
 	}
 	
+	private void appendSynonyms(ResponseWriter responseWriter, SynonymAttribute synonymAttribute) throws ResultWriterException {
+		//synonym extraction
+		if (synonymAttribute!=null) {
+			@SuppressWarnings("rawtypes")
+			List synonyms = synonymAttribute.getSynonyms();
+			if(synonyms != null) {
+				for(Object synonymObj : synonyms) {
+					if(synonymObj instanceof CharVector) {
+						CharVector synonym = (CharVector)synonymObj;
+						responseWriter.value(synonym.toString());
+					} else if(synonymObj instanceof List) {
+						@SuppressWarnings("rawtypes")
+						List synonymList = (List)synonymObj;
+						for(Object synonym : synonymList) {
+							responseWriter.value(synonym.toString());
+						}
+					}
+				}
+			}
+		}
+	}
 }
