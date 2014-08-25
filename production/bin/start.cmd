9bcbf911f13dac5af61ab04750b7564ca6076a13
Sang Song <swsong_dev@websqrd.com>
2014-08-25 11:04:07 +0900

    윈도우 환경에서 검색엔진이 utf-8로 시작하도 스크립트 수정함.

--- production/bin/start.cmd
+++ production/bin/start.cmd
@@ -29,6 +29,6 @@ echo fastcatsearch start. see log at logs/system.log
 
 for /f "tokens=*" %%x in ('dir /s /b %LIB%\*.jar') do (set FASTCAT_CLASSPATH=!FASTCAT_CLASSPATH!;%%x)
 
-java -Xmx512m -Dlogback.configurationFile=%CONF%\logback.xml -Dderby.stream.error.file=logs/db.log -classpath %FASTCAT_CLASSPATH% org.fastcatsearch.server.CatServer %IR_HOME%
+java -Xmx512m -Dlogback.configurationFile=%CONF%\logback.xml -Dfile.encoding=UTF-8 -Dderby.stream.error.file=logs/db.log -classpath %FASTCAT_CLASSPATH% org.fastcatsearch.server.CatServer %IR_HOME%
 
 endlocal
