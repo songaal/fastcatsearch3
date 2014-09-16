b70dee59b94f5e61a7a236dd6889a7f003561a13
Sang Song <swsong_dev@websqrd.com>
2014-09-16 20:36:57 +0900

    플러그인에서 라이선스 파일 검증하는 기능 추가.

---
+++ server/src/main/java/org/fastcatsearch/plugin/LicenseInvalidException.java
@@ -0,0 +1,18 @@
+package org.fastcatsearch.plugin;
+
+/**
+ * Created by swsong on 2014. 9. 16..
+ */
+public class LicenseInvalidException extends Exception {
+
+    public LicenseInvalidException(){
+        super();
+    }
+
+    public LicenseInvalidException(String message) {
+        super(message);
+    }
+
+
+}
+
--- server/src/main/java/org/fastcatsearch/plugin/Plugin.java
+++ server/src/main/java/org/fastcatsearch/plugin/Plugin.java
@@ -1,13 +1,13 @@
 package org.fastcatsearch.plugin;
 
-import java.io.File;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.fastcatsearch.db.InternalDBModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Plugin {
 	protected static final Logger logger = LoggerFactory.getLogger(Plugin.class);
@@ -17,18 +17,42 @@ public abstract class Plugin {
 	protected PluginSetting pluginSetting;
 	protected InternalDBModule internalDBModule;
 	protected boolean isLoaded;
-	
+	protected File licenseFile;
+
 	public Plugin(File pluginDir, PluginSetting pluginSetting){
 		this.pluginDir = pluginDir;
 		this.pluginSetting = pluginSetting;
 		this.pluginId = pluginSetting.getId();
+        this.licenseFile = new File(pluginDir, "license.txt");
 	}
 	
 	public String pluginId(){
 		return pluginId;
 	}
 	
-	public final void load(boolean isMaster){
+	public final void load(boolean isMaster) throws LicenseInvalidException {
+        if(licenseFile != null) {
+
+            InputStream licenseInputStream = null;
+            try{
+                try {
+                    licenseInputStream = new FileInputStream(licenseFile);
+                } catch (FileNotFoundException e) {
+                    //ignore
+                }
+
+                validateLicense(licenseInputStream);
+            } finally {
+                if (licenseInputStream != null) {
+                    try {
+                        licenseInputStream.close();
+                    } catch (IOException e) {
+                        //ignore
+                    }
+                }
+            }
+        }
+
 		boolean isLoadDb = isMaster && pluginSetting.isUseDB();
 		if(isLoadDb){
 			loadDB();
@@ -36,7 +60,15 @@ public abstract class Plugin {
 		doLoad(isLoadDb);
 		isLoaded = true;
 	}
-	
+
+    /**
+    * To be override for license validation
+    * @Param licenseInputStream License file inputstream. It can be null if file is not exist.
+    * */
+    protected boolean validateLicense(InputStream licenseInputStream) throws LicenseInvalidException {
+        return true;
+    }
+
 	public final void unload(){
 		doUnload();
 		if(pluginSetting.isUseDB()){
@@ -45,7 +77,7 @@ public abstract class Plugin {
 		isLoaded = false;
 	}
 	
-	public final void reload(boolean isMaster){
+	public final void reload(boolean isMaster) throws LicenseInvalidException {
 		unload();
 		load(isMaster);
 	}
