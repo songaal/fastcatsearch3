5019dabf4fa60f0ffa82dfe7e5e44042b2eb6373
Sang Song <swsong@fastcat.co>
2015-09-30 16:22:51 +0900

    파일기반해시 개발.

--- core/src/main/java/org/fastcatsearch/ir/io/FileBaseHashSet.java
+++ core/src/main/java/org/fastcatsearch/ir/io/FileBaseHashSet.java
@@ -1,143 +1,131 @@
-///*
-// * Copyright 2013 Websquared, Inc.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *   http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package org.fastcatsearch.ir.io;
-//
-//import org.fastcatsearch.al.HashFunctions;
-//import org.fastcatsearch.ir.common.IRException;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.io.IOException;
-//import java.io.RandomAccessFile;
-//import java.util.Arrays;
-//
-///**
-// * 대용량의 hashset 처리를 위해서 파일기반으로 hashset를 처리하는 클래스.
-// *
-// * */
-//public class FileBaseHashSet<T> {
-//	private static final Logger logger = LoggerFactory.getLogger(FileBaseHashSet.class);
-//
-//	private static final HashFunctions hfunc = HashFunctions.RSHash;
-//
-//    private File f;
-//
-//	private int[] bucket;
-//	private char[] keyArray;
-//	private int[] keyPos;
-//	private int[] nextIdx;
-//
-//	private int bucketSize;
-//	private int length;
-//	private int count;
-//	private int keyArrayLength;
-//	private int keyUseLength;
-//
-//    private RandomAccessFile raf;
-//
-//    private long countOffset;
-//    private long keyPosOffset;
-//    private long nextOffset;
-//    private long bucketOffset;
-//    private long keyArrayOffset;
-//
-//    /**
-//     * 기존파일 오픈
-//     * */
-//    public FileBaseHashSet(File f) throws FileNotFoundException {
-//        raf = new RandomAccessFile(f, "rw");
-//        load();
-//    }
-//
-//    /**
-//     * 새로 생성
-//     * */
-//	public FileBaseHashSet(File f, int bucketSize){
-//        if(f.exists()) {
-//            f.delete();
-//        }
-//
-//        try {
-//            f.createNewFile();
-//            raf = new RandomAccessFile(f, "rw");
-//        } catch (IOException e) {
-//            logger.error("", e);
-//        }
-//        init(bucketSize);
-//	}
-//
-//    public void init(int bucketSize){
-//        this.bucketSize = bucketSize;
-//        length = bucketSize;
-//        count = 0;
-//        keyArrayLength = bucketSize * 5;
-//        keyUseLength = 0;
-//
-//        bucket = new int[bucketSize];
-//        keyArray = new char[keyArrayLength];
-//        keyPos = new int[length];
-//        nextIdx = new int[length];
-//
-//        Arrays.fill(bucket, -1);
-//    }
-//
-//    public void load() {
-//
-//    }
-//
-////	public FileBaseHashSet(File file) throws IRException{
-////		this.file = file;
-////		try {
-////			long st = System.currentTimeMillis();
-////
-////			IndexInput input = new BufferedFileInput(file);
-////			bucketSize = input.readInt();
-////			count = input.readInt();
-////			keyUseLength = input.readInt();
-////
-////			bucket = new int[bucketSize];
-////			keyArray = new char[keyUseLength];
-////			keyPos = new int[count];
-////			nextIdx = new int[count];
-////
-////			for (int i = 0; i < bucketSize; i++) {
-////				bucket[i] = input.readInt();
-////			}
-////			for (int i = 0; i < keyUseLength; i++) {
-////				keyArray[i] = input.readUChar();
-////			}
-////			for (int i = 0; i < count; i++) {
-////				keyPos[i] = input.readInt();
-////			}
-////			for (int i = 0; i < count; i++) {
-////				nextIdx[i] = input.readInt();
-////			}
-////			input.close();
-////
-////			logger.info("Load dictionary done! {}, entry = {}, time = {}ms", new Object[]{file.getAbsolutePath(), count, (System.currentTimeMillis() - st)});
-////		} catch (IOException e) {
-////			logger.error("IOException",e);
-////			throw new IRException(e);
-////		}
-////
-////	}
-//
+/*
+ * Copyright 2013 Websquared, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fastcatsearch.ir.io;
+
+import org.fastcatsearch.al.HashFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * 대용량의 hashset 처리를 위해서 파일기반으로 hashset를 처리하는 클래스.
+ * key는 고정길이 String이다.
+ *
+ * */
+public class FileBaseHashSet {
+	private static final Logger logger = LoggerFactory.getLogger(FileBaseHashSet.class);
+
+	private static final HashFunctions hfunc = HashFunctions.RSHash;
+
+    private File f;
+
+	private int bucketSize;
+	private int count;
+
+    private RandomAccessFile raf;
+
+    private long bucketSizeOffset = 0L;
+    private long keySizeOffset = 4L;
+    private long countOffset = 8L;
+    private long bucketOffset = 12L;
+    private long nextOffset;
+    private long keyPosOffset;
+    private long keyArrayOffset;
+
+    private static final long HEADER_LENGTH = 12L;
+    private static int KEY_WIDTH;
+    private static final int BUCKET_WIDTH = 4;
+    private static final int NEXT_WIDTH = 4;
+    private static final int KEYPOS_WIDTH = 8;
+
+    private int SEGMENT_ENTRY_SIZE;
+    private int BUCKET_LENGTH;
+    private int SEGMENT_WIDTH;
+    private int limit;
+
+    /**
+     * 기존파일 오픈
+     * */
+    public FileBaseHashSet(File f) throws IOException {
+        raf = new RandomAccessFile(f, "rw");
+        load();
+    }
+    public void load() throws IOException {
+        int bucketSize = raf.readInt();
+        int keySize = raf.readInt();
+        int count = raf.readInt();
+        init(bucketSize, keySize, count);
+    }
+    /**
+     * 새로 생성
+     * */
+	public FileBaseHashSet(File f, int bucketSize, int keySize){
+        if(f.exists()) {
+            f.delete();
+        }
+
+        try {
+            f.createNewFile();
+            raf = new RandomAccessFile(f, "rw");
+
+            int count = 0;
+            init(bucketSize, keySize, count);
+            raf.seek(0);
+            raf.writeInt(bucketSize);
+            raf.writeInt(keySize);
+            raf.writeInt(count);
+            spanFileToSegment(0);
+
+            raf.seek(12);
+            int tmp = raf.readUnsignedShort();
+
+            logger.debug("tmp = {}", tmp);
+        } catch (IOException e) {
+            logger.error("", e);
+        }
+	}
+
+    public void init(int bucketSize, int keySize, int count){
+        this.bucketSize = bucketSize;
+        SEGMENT_ENTRY_SIZE = bucketSize;
+        KEY_WIDTH = keySize;
+        nextOffset = bucketOffset + BUCKET_WIDTH * SEGMENT_ENTRY_SIZE;
+        keyPosOffset = nextOffset + NEXT_WIDTH * SEGMENT_ENTRY_SIZE;
+        keyArrayOffset = keyPosOffset + KEYPOS_WIDTH * SEGMENT_ENTRY_SIZE;
+
+        BUCKET_LENGTH = BUCKET_WIDTH * SEGMENT_ENTRY_SIZE;
+        SEGMENT_WIDTH = (NEXT_WIDTH + KEYPOS_WIDTH + KEY_WIDTH) * SEGMENT_ENTRY_SIZE;
+        this.count = count;
+        this.limit = (count / SEGMENT_ENTRY_SIZE + 1) * SEGMENT_ENTRY_SIZE; //엔트리 제한 갯수. 이 수치를 넘어서면 파일길이를 늘린다.
+    }
+
+    private void spanFileToSegment(int segment) {
+        try {
+            raf.setLength(HEADER_LENGTH + BUCKET_LENGTH + segment + 1 * SEGMENT_WIDTH);
+        } catch (IOException e) {
+            logger.error("", e);
+        }
+    }
+
+
+
 //	public void save(File file) throws IRException{
 //		try {
 //			IndexOutput output = new BufferedFileOutput(file);
@@ -164,140 +152,123 @@
 //			output.close();
 //
 ////			logger.info("Wrote {}, {}", Formatter.getFormatSize(output.size()), file.getAbsolutePath());
-//			logger.info("Wrote {}, {}", file.length(), file.getAbsolutePath());
+//            logger.info("Wrote {}, {}", file.length(), file.getAbsolutePath());
 //		} catch (IOException e) {
 //			logger.error("IOException",e);
 //			throw new IRException(e);
 //		}
 //	}
-//
-//	private boolean isTheSame(CharVector term, int id) {
-//		int pos = keyPos[id];
-//		int len = -1;
-//		//last el?
-//		if(id == count - 1)
-//			len = keyUseLength - pos;
-//		else
-//			len = keyPos[id+1] - pos;
-//
-//		if(term.length() == len){
-//			for (int i = 0; i < len; i++) {
-//				if(term.charAt(i) != keyArray[pos+i])
-//					return false;
-//			}
-//			return true;
-//		}
-//		return false;
-//	}
-//
-//	public boolean put(T key) {
-////		logger.debug("term >> {}", term);
-//		int hashValue = key.hashCode() % bucketSize;
-//
-//		int prev = -1;
-//		int idx = bucket[hashValue];
-//
-//		while(idx >= 0){
-//			if(isTheSame(key, idx))
-//				break;
-//
-//			prev = idx;
-//			idx = nextIdx[idx];
-//		}
-////		logger.debug("idx = "+idx);
-//		if(idx >= 0){
-//			//duplicated term
-//			if(prev != -1){
-//				//put a link to the front
-//				nextIdx[prev] = nextIdx[idx];
-//				nextIdx[idx] = bucket[hashValue];
-//				bucket[hashValue] = idx;
-//			}//else let it be
-//		}
-//		else{
-//			//new term
-//			idx = getNextIdx();
-////			logger.debug("NextIdx = "+idx+" , term.length="+term.length);
-//			if (keyUseLength + term.length() >= keyArrayLength) {
-//				keyArrayLength *= 1.2;
-//				char[] newArray = new char[keyArrayLength];
-//				System.arraycopy(keyArray, 0, newArray, 0, keyUseLength);
-//				keyArray = newArray;
-//			}
-//			keyPos[idx] = keyUseLength;
-//
-//			for (int i=0; i < key.length(); i++) {
-//				keyArray[keyUseLength++] = key.charAt(i);
-////				logger.debug("t = "+keyArray[keyUseLength - 1]);
-//			}
-//
-//			nextIdx[idx] = -1;
-//			if(prev != -1)
-//				nextIdx[prev] = idx;
-//			else
-//				bucket[hashValue] = idx;
-//
-//		}
-//
-//		return true;
-//	}
-//
-//	public boolean contains(CharVector term) {
-//		int hashValue = hfunc.hash(term, bucketSize);
-//		int idx = bucket[hashValue];
-//
-////		logger.debug(term+" = "+hashValue+", idx="+idx);
-//		while(idx >= 0){
-//			if(isTheSame(term, idx))
-//				break;
-//
-//			idx = nextIdx[idx];
-//		}
-//
-//		if(idx < 0)
-//			return false; //검색실패
-//		else{
-//			return true;
-//		}
-//	}
-//
-//	private int getNextIdx() {
-//		if(count >= length){
-//			int newLength = (int) (length * 1.2);
-////			logger.debug("Grow length = "+length+" => "+newLength+", new int * 2, new PostingBuffer[], arraycopy * 3");
-//			int[] newKeyPos = new int[newLength];
-//			int[] newNext = new int[newLength];
-//
-//			System.arraycopy(keyPos, 0, newKeyPos, 0, count);
-//			System.arraycopy(nextIdx, 0, newNext, 0, count);
-//
-//			keyPos = newKeyPos;
-//			nextIdx = newNext;
-//			length = newLength;
-//		}
-//		return count++;
-//	}
-//
-//	public int staticMemorySize(){
-//		int size = 0;
-//		size += keyArrayLength * 2;
-//		size += bucket.length * 4;
-//		size += keyPos.length * 4;
-//		size += nextIdx.length * 4;
-//
-//		return size;
-//	}
-//
-//	public void clear(){
-//		Arrays.fill(bucket, -1);
-//		Arrays.fill(nextIdx, -1);
-//		count = 0;
-//		keyUseLength = 0;
-//	}
-//
-//	//entry count
-//	public int count() {
-//		return count;
-//	}
-//
-//}
+
+
+    private long readKeyPos(int id) throws IOException {
+        int segment = id / SEGMENT_ENTRY_SIZE;
+        long offset = keyPosOffset + segment * SEGMENT_ENTRY_SIZE + id * KEY_WIDTH;
+        raf.seek(offset);
+        return raf.readLong();
+    }
+	private boolean isTheSame(String term, int id) throws IOException {
+		long pos = readKeyPos(id);
+        raf.seek(pos);
+        for (int i = 0; i < KEY_WIDTH; i++) {
+            if(term.charAt(i) != raf.readChar())
+                return false;
+        }
+        return true;
+	}
+
+    private int readNextIndex(int id) throws IOException {
+        int segment = id / SEGMENT_ENTRY_SIZE;
+        long offset = nextOffset + segment * SEGMENT_ENTRY_SIZE + id * NEXT_WIDTH;
+        raf.seek(offset);
+        return raf.readInt();
+    }
+    private void writeNextIndex(int id, int nextId) throws IOException {
+        int segment = id / SEGMENT_ENTRY_SIZE;
+        long offset = nextOffset + segment * SEGMENT_ENTRY_SIZE + id * NEXT_WIDTH;
+        raf.seek(offset);
+        raf.writeInt(nextId);
+    }
+    private int readBucket(int hashValue) throws IOException {
+        long offset = bucketOffset + hashValue * BUCKET_WIDTH;
+        raf.seek(offset);
+        return raf.readInt();
+    }
+
+    private void writeBucket(int hashValue, int id) throws IOException {
+        long offset = bucketOffset + hashValue * BUCKET_WIDTH;
+        raf.seek(offset);
+        raf.writeInt(id);
+    }
+
+	public boolean put(String key) {
+        try {
+//		    logger.debug("term >> {}", term);
+            int hashValue = (key.hashCode() % (bucketSize - 1)) + 1; //0을 피한다.
+
+            int prev = 0;
+            int idx = readBucket(hashValue);
+
+            while (idx > 0) {
+                if (isTheSame(key, idx)) {
+                    //동일 발견시 false리턴.
+                    return false;
+                }
+
+                prev = idx;
+                idx = readNextIndex(idx);
+            }
+            idx = getNextIdx();
+            logger.debug("NextIdx = {}, key = {}", idx, key);
+
+            long offset = readKeyPos(idx);
+            raf.seek(offset);
+            for (int i = 0; i < key.length(); i++) {
+                raf.writeChar(key.charAt(i));
+            }
+
+            if (prev > 0) {
+//                nextIdx[prev] = idx;
+                writeNextIndex(prev, idx);
+            } else {
+//                bucket[hashValue] = idx;
+                writeBucket(hashValue, idx);
+            }
+            return true;
+        }catch(Exception e) {
+            logger.error("", e);
+            return false;
+        }
+	}
+
+	public boolean contains(String key) {
+        try {
+            int hashValue = (key.hashCode() % (bucketSize - 1)) + 1; //0을 피한다.
+            int idx = readBucket(hashValue);
+
+            while(idx > 0){
+                if(isTheSame(key, idx)){
+                    return true;
+                }
+                idx = readNextIndex(idx);
+            }
+
+            return false;
+        }catch(Exception e) {
+            logger.error("", e);
+            return false;
+        }
+	}
+
+	private int getNextIdx() {
+        if(count >= limit) {
+            spanFileToSegment(limit / bucketSize);
+        }
+		return count++;
+	}
+
+	//entry count
+	public int count() {
+		return count;
+	}
+
+}
--- core/src/test/java/org/fastcatsearch/ir/io/FileBaseHashSetTest.java
+++ core/src/test/java/org/fastcatsearch/ir/io/FileBaseHashSetTest.java
@@ -1,20 +1,21 @@
-//package org.fastcatsearch.ir.io;
-//
-//import org.junit.Test;
-//
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//
-///**
-// * Created by swsong on 2015. 9. 24..
-// */
-//public class FileBaseHashSetTest {
-//
-//    @Test
-//    public void testInsert() throws FileNotFoundException {
-//        File f = new File("/tmp/a");
-//        FileBaseHashSet<String> set = new FileBaseHashSet<String>(f);
-//
-//        set.put()
-//    }
-//}
+package org.fastcatsearch.ir.io;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by swsong on 2015. 9. 24..
+ */
+public class FileBaseHashSetTest {
+
+    @Test
+    public void testNew() throws FileNotFoundException {
+        File f = new File("/tmp/a");
+        int bucketSize= 10;
+        int keySize = 2;
+        FileBaseHashSet set = new FileBaseHashSet(f, bucketSize, keySize);
+
+    }
+}
