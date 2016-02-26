ad2bc6c914c9d6d9a048b5beefe9974fbecca778
Sang Song <swsong@fastcat.co>
2016-02-26 17:52:18 +0900

    jon list source reader 빠르게

--- server/src/main/java/org/fastcatsearch/datasource/reader/JSONListFileReader.java
+++ server/src/main/java/org/fastcatsearch/datasource/reader/JSONListFileReader.java
@@ -3,10 +3,12 @@ package org.fastcatsearch.datasource.reader;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.minidev.json.parser.ParseException;
 import org.fastcatsearch.datasource.SourceModifier;
 import org.fastcatsearch.datasource.reader.annotation.SourceReader;
 import org.fastcatsearch.ir.common.IRException;
 import org.fastcatsearch.ir.config.SingleSourceConfig;
+import org.fastcatsearch.util.JSONParser;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -14,27 +16,36 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@SourceReader(name="JSON_LIST_FILE")
+@SourceReader(name = "JSON_LIST_FILE")
 public class JSONListFileReader extends AbstractFileReader {
 
+    private JSONParser jsonParser;
+
     public JSONListFileReader() {
+        jsonParser = new JSONParser();
     }
 
     public JSONListFileReader(String collectionId, File filePath, SingleSourceConfig singleSourceConfig, SourceModifier sourceModifier, String lastIndexTime) throws IRException {
         super(collectionId, filePath, singleSourceConfig, sourceModifier, lastIndexTime);
+        jsonParser = new JSONParser();
     }
 
     @Override
     protected Map<String, Object> parse(BufferedReader reader) throws IRException, IOException {
         String line = reader.readLine();
-        if(line == null) {
+        if (line == null) {
             throw new IOException("EOF");
         }
-
-        JsonFactory jsonFactory = new JsonFactory();
-        ObjectMapper mapper = new ObjectMapper(jsonFactory);
-        TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>(){};
-        return mapper.readValue(line, typeRef);
+        line = line.trim();
+        if (line.length() == 0) {
+            return null;
+        }
+        try {
+            return jsonParser.parse(line);
+        } catch (ParseException e) {
+            logger.error("error while convert json to map : " + line, e);
+        }
+        return null;
     }
 
     @Override
