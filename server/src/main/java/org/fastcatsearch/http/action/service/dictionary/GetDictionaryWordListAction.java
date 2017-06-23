6fa4c3ca41c4f2cc43c284dded3dbf195774b365
FreshBread <white_len@naver.com>
2017-06-23 13:51:20 +0900

    사전 서비스 API 중 단어 리스트를 요청하는 API의 Request Method를 POST로도 사용할 수 있도록 수정

--- server/src/main/java/org/fastcatsearch/http/action/service/dictionary/GetDictionaryWordListAction.java
+++ server/src/main/java/org/fastcatsearch/http/action/service/dictionary/GetDictionaryWordListAction.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@ActionMapping(value="/service/dictionary/list", method = { ActionMethod.GET })
+@ActionMapping(value="/service/dictionary/list", method = { ActionMethod.GET, ActionMethod.POST })
 public class GetDictionaryWordListAction extends ServiceAction {
 
     @Override
